518d40aae9ae1456de7fff2f4bc231d6e90f4dbd
Alef Gaigher <alef@adaline.com.br>
2024-07-06 17:04:05 -0300

    autenticacao

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,44 +1,34 @@
-import NextAuth from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials"
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 const handler = NextAuth({
-    pages:{
-        signIn: "/"
-    },
-    providers: [
-        CredentialsProvider({
-          name: 'Credentials',
-          credentials: {
-            email: { label: "Email", type: "email" },
-            password: { label: "Password", type: "password" }
-          },
-          async authorize(credentials, req) {
-            if (!credentials){
-                return null;
-            }
+  pages: {
+    signIn: "/",
+  },
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: async (credentials, req) => {
+        const email = credentials?.email;
+        const password = credentials?.password;
 
-            if(credentials.email === "alefgaigher@hotmail.com" && credentials.password === "123"){
-                return {
-                    id: "1",
-                    name: "alef",
-                    email: "alefgaigher@hotmail.com"
-                }
-            }
-            // const res = await fetch("/your/endpoint", {
-            //   method: 'POST',
-            //   body: JSON.stringify(credentials),
-            //   headers: { "Content-Type": "application/json" }
-            // })
-            // const user = await res.json()
-            // if (res.ok && user) {
-            //   return user
-            // }
-            // return null
-
-            return null;
-          },
-        }),
-      ],
+        const res = await fetch(`http://localhost:8001/users?email=${email}&senha=${password}`, {
+          method: 'GET',
+          headers: { 'Content-Type': 'application/json' }
+        });
+        const users = await res.json();
+        if (res.ok && users.length > 0) {
+          return users[0];
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
 });
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
